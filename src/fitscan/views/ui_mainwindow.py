b36967cb23f1a3f563744469737f6e79927a0e17
GunfighterJ <joseph.jenniges@gmail.com>
2016-07-24 20:33:44 -0500

    Update resources

--- src/fitscan/views/ui_mainwindow.py
+++ src/fitscan/views/ui_mainwindow.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'resources\ui\mainwindow.ui'
 #
-# Created: Sun Jul 24 20:12:10 2016
+# Created: Sun Jul 24 20:33:29 2016
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
